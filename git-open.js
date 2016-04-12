a5b257bdca6a6d80a57b276a81720d30bcbabc78
jbradley <jbradley@hubspot.com>
2016-04-12 11:50:01 -0400

    basic functionality

--- git-open.js
+++ git-open.js
@@ -2,10 +2,39 @@
 
 'use strict';
 
-var exec = require('child_process').exec;
+var shelljs = require('shelljs');
+var open = require('open');
 
-function getBranchName() {
-  return exec("git rev-parse --abbrev-ref HEAD");
+const gitBranchCommand = "git rev-parse --abbrev-ref HEAD";
+const gitRemotOriginUrlCommand = "git config remote.origin.url";
+
+function buildGitRepositoryUrl(gitBranchName, gitBaseUrl) {
+  if ("master" == gitBranchName) {
+    return gitBaseUrl;
+  }
+  return gitBaseUrl + gitBranchName;
 }
 
-console.log(getBranchName());
+function getGitBranchName() {
+  var branchName = shelljs.exec(gitBranchCommand).stdout;
+  return branchName.trim();
+}
+
+function getGitRemoteOriginUrl() {
+  var remoteOriginUrl = shelljs.exec(gitRemotOriginUrlCommand).stdout;
+  return remoteOriginUrl.trim();
+}
+
+function getGitBaseUrl(gitRemoteOriginUrl) {
+  return gitRemoteOriginUrl.substring(0, gitRemoteOriginUrl.lastIndexOf("."));
+}
+
+function openGitRepositoryPage() {
+  const gitBranchName = getGitBranchName();
+  const gitRemoteOriginUrl = getGitRemoteOriginUrl();
+  const gitBaseUrl = getGitBaseUrl(gitRemoteOriginUrl);
+  const gitRepositoryUrl = buildGitRepositoryUrl(gitBranchName, gitBaseUrl);
+  open(gitRepositoryUrl);
+}
+
+openGitRepositoryPage();
